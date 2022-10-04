03247c28ebc89a3d40d65b9e3d32951183f6e838
Innocent Makusa <innmak@proton.me>
2022-10-04 16:16:19 +0200

    editted media query .panel

--- 1. Expanding cards/script.js
+++ 1. Expanding cards/script.js
@@ -1,9 +1,13 @@
 const panels = document.querySelectorAll('.panel')
+/* const mediaQuery = window.matchMedia('(max-width:480px)') */
 
 panels.forEach((panel)=>{
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
+        /*if (mediaQuery.matches){
+
+        }*/
     })
 })
 
